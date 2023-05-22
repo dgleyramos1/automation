3e31fc5a9c00def4f3b567474d869f957de927d1
Dgley ramos <dgleysilva@gmail.com>
2023-05-22 03:05:52 -0300

    messages: adicionandos mensagens para atentende e cardapio

---
+++ src/messages/atendenteMessage.js
@@ -0,0 +1,6 @@
+
+
+
+export const atendenteMessage = () => {
+    return '*Você será encaminhado para conversar com um de nossos atendentes, por favor, aguarde!*'
+}
---
+++ src/messages/cardapioMessage.js
@@ -0,0 +1,5 @@
+
+
+export const cardapioMessage = () => {
+    return "*Cardápio*\n\nEscolha um dos nossos tópicos para mostrarmos os nossos produtos.\n\n1 - Artesanais\n2 - Hamburgueres\n3 - Lanches frios\n4 - Lanches Tradicionais\n5 - Beiruth's\n6 - Frituras\n7 - Milk Shakes\n8 - Açaís\n9 - Creme de Frutas\n10 - Combos"
+}
